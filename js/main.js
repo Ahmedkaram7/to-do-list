3b31e2b254fcbe1ac7c9dfc8d00bdb00800ce21d
Ahmed Karam <ahmedkaramali710@outlook.com>
2024-09-17 18:39:02 +0300

    save tasks in localsorage and show in page

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ let addBtn = document.getElementById("add");
 
 addBtn.addEventListener("click", addTask);
 
+// add task in list
 function addTask() {
   if (inputBox.value === "") {
     alert("Write Something");
@@ -12,7 +13,7 @@ function addTask() {
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
     // console.log("Error");
-    console.log(li);
+    // console.log(li);
     listContainer.appendChild(li);
     let span = document.createElement("span");
     span.innerHTML = "\u00d7";
@@ -20,6 +21,7 @@ function addTask() {
     // console.log("Error");
   }
   inputBox.value = "";
+  saveData();
 }
 
 listContainer.addEventListener(
@@ -27,9 +29,19 @@ listContainer.addEventListener(
   (el) => {
     if (el.target.tagName === "LI") {
       el.target.classList.toggle("cheched");
+      saveData();
     } else if (el.target.tagName === "SPAN") {
       el.target.parentElement.remove();
+      saveData();
     }
   },
   false
 );
+
+function saveData() {
+  localStorage.setItem("task", listContainer.innerHTML);
+}
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("task");
+}
+showTask();
